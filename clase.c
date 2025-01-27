c96257c6a52af31cd1dc10386893642567e516fc
Mishicoder <56014494+mishicoder@users.noreply.github.com>
2025-01-27 10:45:10 -0500

    create

---
+++ clase.c
@@ -0,0 +1,6 @@
+/*
+Código para la clase.
+
+Link de Tiled: https://drive.google.com/file/d/1pf-bfHFhksNBlDmHYR_yMXHU_Dtn6Xut/view?usp=drive_link
+Link de Libre Sprite: https://drive.google.com/file/d/1sMko1td4R7VhTx0uipYTPVjJtDWzc9FR/view?usp=drive_link
+*/
